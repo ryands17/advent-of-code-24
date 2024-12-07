b3c2a6c45c167d60ac39b1cd77769737e6659c64
Ryan Dsouza <dsouza.ryan.11@gmail.com>
2024-12-08 00:24:09 +0100

    feat: complete day 07

---
+++ day-07/src/part2.rs
@@ -0,0 +1,96 @@
+enum Operation {
+  Add,
+  Multiply,
+  Concatenate,
+}
+
+pub fn process(input: &str) -> usize {
+  input
+    .lines()
+    .filter_map(|line| {
+      let mut sp = line.split(':');
+      let target = sp.next().unwrap().parse::<usize>().unwrap();
+
+      let values = sp
+        .next()
+        .unwrap()
+        .trim()
+        .split(' ')
+        .map(|v| v.parse::<usize>().unwrap())
+        .collect::<Vec<_>>();
+
+      if is_target_match(target, &values, 0) {
+        return Some(target);
+      }
+
+      None
+    })
+    .sum()
+}
+
+fn is_target_match(target: usize, list: &[usize], current: usize) -> bool {
+  if list.is_empty() {
+    return target == current;
+  }
+
+  if current > target {
+    return false;
+  }
+
+  if is_target_match(
+    target,
+    &list[1..],
+    operate(current, list[0], Operation::Add),
+  ) {
+    return true;
+  }
+
+  if is_target_match(
+    target,
+    &list[1..],
+    operate(current, list[0], Operation::Concatenate),
+  ) {
+    return true;
+  }
+
+  let current = if current == 0 { 1 } else { current };
+  is_target_match(
+    target,
+    &list[1..],
+    operate(current, list[0], Operation::Multiply),
+  )
+}
+
+fn operate(n1: usize, n2: usize, op: Operation) -> usize {
+  match op {
+    Operation::Add => n1 + n2,
+    Operation::Multiply => n1 * n2,
+    Operation::Concatenate => {
+      if n1 == 0 {
+        return n2;
+      }
+
+      format!("{n1}{n2}").parse::<usize>().unwrap()
+    }
+  }
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn sample_input() {
+    let input = r"190: 10 19
+3267: 81 40 27
+83: 17 5
+156: 15 6
+7290: 6 8 6 15
+161011: 16 10 13
+192: 17 8 14
+21037: 9 7 18 13
+292: 11 6 16 20";
+
+    assert_eq!(11387, process(input));
+  }
+}
